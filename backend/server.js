596a121727f8e91d6aa36af528d43ece83d7b735
LegendarySumit <shirotprusty4444@gmail.com>
2026-09-01 22:29:57 +0530

    Update backend dependencies and server configuration

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+require('dotenv').config();
+
 const cors = require("cors");
 const path = require("path");
 const { spawn, execSync } = require("child_process");
